4faf41f3f890d8e53a08bd00835293654b5c9d54
Marjona Abdukhalikova <marjonaabdukhalikova@Diors-MacBook-Air.local>
2026-01-27 21:15:24 +0500

    test

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,16 @@
-// var swiper = new Swiper(".mySwiper", {
 
+// var swiper = new Swiper(".mySwiper", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: 3,
+//     coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
 //     pagination: {
 //         el: ".swiper-pagination",
 //     },
@@ -7,24 +18,28 @@
 //         nextEl: ".swiper-button-next",
 //         prevEl: ".swiper-button-prev",
 //     },
-//     // slidesPerView: 3,
-//     // spaceBetween: 2,
-//     clickable: true,
 // });
+
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         navigation: {
+//             nextEl: ".swiper-button-next",
+//             prevEl: ".swiper-button-prev",
+//         },
+//         renderBullet: function (index, className) {
+//             return '<span class="' + className + '">' + (index + 1) + "</span>";
+//         },
+//     },
+// });
+
+
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
+    spaceBetween: 30,
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
         nextEl: ".swiper-button-next",
