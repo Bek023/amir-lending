cf3f5b8328987c63ce59955bad243c9181a5e004
Amir Abdukhalikov <amirjonabduxoliqov222@gmail.com>
2026-02-06 22:13:44 +0500

    api update

--- js/modal.js
+++ js/modal.js
@@ -1,81 +1,82 @@
-const btn = document.querySelector('.main__btn');
-const bigLabel = document.querySelector('.big__label');
-const content = document.querySelector('.content');
+const btn = document.querySelector(".main__btn");
+const bigLabel = document.querySelector(".big__label");
+const content = document.querySelector(".content");
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
 function fncOpen() {
-    bigLabel.classList.add('big__container-active');
-    content.classList.add('content-active');
-    header.classList.add('d__none');
+  bigLabel.classList.add("big__container-active");
+  content.classList.add("content-active");
+  header.classList.add("d__none");
 }
 function fncClose() {
-    bigLabel.classList.remove('big__container-active');
-    content.classList.remove('content-active');
-    header.classList.remove('d__none');
+  bigLabel.classList.remove("big__container-active");
+  content.classList.remove("content-active");
+  header.classList.remove("d__none");
 }
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-        fncClose();
-    }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    fncClose();
+  }
 });
 
-// 
+//
 
 function validation(form) {
-    function removeErrors(input) {
-        const parent = input.parentNode;
-        if (parent.classList.contains('error')) {
-            parent.querySelector('.error-label').remove();
-            parent.classList.remove('error');
-        }
+  function removeErrors(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains("error")) {
+      parent.querySelector(".error-label").remove();
+      parent.classList.remove("error");
     }
+  }
 
-    let result = true;
-    function createError(input, text) {
-        const parent = input.parentNode;
-        const errorLabel = document.createElement('p');
-        errorLabel.classList.add('error-label');
-        errorLabel.textContent = text;
-        parent.append(errorLabel);
-        parent.classList.add('error');
-    }
+  let result = true;
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement("p");
+    errorLabel.classList.add("error-label");
+    errorLabel.textContent = text;
+    parent.append(errorLabel);
+    parent.classList.add("error");
+  }
 
-    const AllInputs = form.querySelectorAll('input');
-    for (const input of AllInputs) {
+  const AllInputs = form.querySelectorAll("input");
+  for (const input of AllInputs) {
+    removeErrors(input);
+
+    if (input.dataset.minLength) {
+      if (input.value.length < input.dataset.minLength) {
         removeErrors(input);
-
-        if (input.dataset.minLength) {
-            if (input.value.length < input.dataset.minLength) {
-                removeErrors(input);
-                createError(
-                    input,
-                    `Минимальная длина ${input.dataset.minLength} символов`,
-                );
-                result = false;
-            }
-        }
-
-        if (input.value == '') {
-            removeErrors(input);
-            createError(input, 'Заполните поле');
-            result = false;
-        }
+        createError(
+          input,
+          `Минимальная длина ${input.dataset.minLength} символов`
+        );
+        result = false;
+      }
     }
 
-    return result;
+    if (input.value == "") {
+      removeErrors(input);
+      createError(input, "Заполните поле");
+      result = false;
+    }
+  }
+
+  return result;
 }
 
-document.getElementById('auth').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const formData = new FormData(document.forms.auth);
-    if (validation(this) == true) {
-        fetch('http://localhost:3000/feedback', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-        // fncClose();
-    }
+document.getElementById("auth").addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("sukes");
+  const formData = new FormData(document.forms.auth);
+  if (validation(this) == true) {
+    fetch("https://api.diamond-dryfruits.com/feedback", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    // fncClose();
+  }
 });
